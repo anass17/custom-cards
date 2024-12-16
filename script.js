6268cf232dc89f0e6e175c951566ff2eb4777f00
anass17 <anassboutaib2018@gmail.com>
2024-12-16 01:02:25 +0100

    Added function to move card elements

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let card = document.querySelector('.card');
 let contextmenu = null;
 let clickedX = 0;
 let clickedY = 0;
+let selectedElement = null;
 
 card.addEventListener('contextmenu', function (e) {
     e.preventDefault();
@@ -32,19 +33,33 @@ card.addEventListener('contextmenu', function (e) {
     contextmenu.querySelector('.context-add-text').addEventListener('click', function () {
 
         let textInput = document.createElement('textarea');
-        textInput.className = `absolute top-[${clickedY}px] left-[${clickedX}px] resize-none h-[30px]`;
-        textInput.value = "Write Here";
+        textInput.className = `absolute top-[${clickedY}px] left-[${clickedX}px] resize-none h-[30px] bg-transparent select-none`;
+        textInput.dataset.color = "000000";
+        textInput.dataset.background = "transparent";
+        // textInput.dataset.font = "Arial";
+        textInput.placeholder = "Write Here";
         document.body.append(textInput);
 
         contextmenu.remove();
         contextmenu = null;
         document.removeEventListener('click', removeContextMenu);
 
+        textInput.addEventListener('mousedown', function () {
+            selectedElement = this;
+            e.preventDefault();
+            card.addEventListener('mousemove', moveElements);
+        });
+
+        textInput.addEventListener('mouseup', function () {
+            card.removeEventListener('mousemove', moveElements);
+        })        
 
     });
 
     document.addEventListener('click', removeContextMenu);
 });
+
+// Remove the context menu
 
 function removeContextMenu(e) {
     if (e.target != contextmenu && !contextmenu.contains(e.target)) {
@@ -52,4 +67,12 @@ function removeContextMenu(e) {
         contextmenu = null;
         document.removeEventListener('click', removeContextMenu);
     }
+}
+
+// Move cards Elements: Text or Images
+
+function moveElements(e) {
+    // selectedElement.setAttribute('disabled', '');
+    selectedElement.style.left = e.x + "px";
+    selectedElement.style.top = e.y + "px";
 }
